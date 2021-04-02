b44b9c6e30a9c50f0dd4a66229b202b3222fe221
Raymond Liu <kuangyu@synopsys.com>
2021-04-02 18:22:34 +0800

    init

--- lambda-hw2/index.py
+++ lambda-hw2/index.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello world! Hello TW.')
+        'body': json.dumps('Hello world! Hello Raymond!')
     }
