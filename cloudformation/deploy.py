6d6754341cbcd37191f208c67ac00ebd286be1d9
Raymond Liu <kuangyu@synopsys.com>
2021-04-02 17:43:18 +0800

    init

---
+++ cloudformation/deploy.py
@@ -0,0 +1,25 @@
+import os
+import subprocess
+from configparser import ConfigParser
+import shlex
+# import boto3
+
+config = ConfigParser()
+config.optionxform = str # # preserve case
+config.read(os.path.join(os.path.dirname(__file__), 'parameters.ini'))
+
+parameters = []
+# [('prefix', 'hw2-cf'), ('appversion', '1.0.0-9')]
+for item in config.items('Parameters'):
+    key = item[0]
+    val = item[1]
+    parameters.append(f'{key}={val}')
+
+template_file = os.path.join(os.path.dirname(__file__), 'hw2.yml')
+param_strs = ' '.join(parameters)
+cmd = (
+    f'aws cloudformation deploy --stack-name hw2 --template-file {template_file} '
+    f'--capabilities CAPABILITY_IAM CAPABILITY_NAMED_IAM --parameter-overrides {param_strs}'
+)
+print('deploy command: ' + cmd)
+subprocess.check_call(shlex.split(cmd))
